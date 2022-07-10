c352daf9980936ad6f6ae6f08abc1634704d6e4f
Evelyn Arevalo <evelynarevalow@gmail.com>
2022-07-10 11:00:55 -0700

    still working on this
    Still working on this project

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,31 @@
 const url = "https://randomuser.me/api/?results=12&inc=name,picture,email,location,&noinfo&nat=US"
 const searchContainer = document.querySelector(".search-container");
-const gallery = document.getElementById('gallery');   
-let body = document.querySelector('body')
+const gallery = document.getElementById('gallery'); 
+let users = [];  
 
 
-//users to display, for loop shows 12 users and allows html and JavaSript to interact w/ each other dynamically
+Promise.all([fetchUserData(url)])
+.then(data => {
+  users = data[0].results
+  displayUsers(users);
+});
 
-function usersToDisplay(user){
+
+async function fetchUserData(url){
+  try {
+    const response = await fetch(url);
+    const response_1 = (response);
+    return response_1.json();
+  } catch (err) {
+    return console.log('SOMETHING WENT WRONG!', err);
+  }
+};
+
+
+function displayUsers(user){
  for(let i = 0 ; i < user.length; i++ ){
-  let html = 
+  let html =
+
               ` <div class="card">
                 <div class="card-img-container">
                         <img class="card-img" src=${user[i].picture.large} alt="profile picture">
@@ -19,61 +36,66 @@ function usersToDisplay(user){
                         <p class="card-text cap">${user[i].location.city} , ${user[i].location.state}</p>
                     </div>`
 
-                   
-
-
- gallery.insertAdjacentHTML('beforeend', html);
+    gallery.insertAdjacentHTML('beforeend', html);
     }
 }
 
-fetch(url)
-.then(response => response.json())
-.then(data => data.results)
-.then(usersToDisplay)
-.catch((err) => console.log('SOMETHING WENT WRONG!'));
-
 // Modula 
+function createModal(index){
+  const { 
+     name,
+     dob, 
+     phone,
+     email,
+     location: {city, street, state, postcode},
+     picture,
+ } = users[index] //Object destructuring 
+
+   const modalContainer = document.querySelector(".modal-cointainer")
+   let modalHtml = 
+
+       ` <div class="modal">
+         <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+         <div class="modal-info-container">
+            <img class="modal-img" src=${picture.medium} alt="profile picture">
+            <h3 id="name" class="modal-name cap"></h3>
+            <p class="modal-text"></p>
+            <p class="modal-text cap"></p>
+            <hr>
+            <p class="modal-text">${phone}</p>
+            <p class="modal-text">${street}, ${city}, ${state} ${postcode}</p>
+            <p class="modal-text">Birthday: 10/21/2015</p>
+             </div>`;
+     
+         document.body.insertAdjacentHTML('beforeend', modalHtml);
+           
+    modalContainer.innerHTML = modalHtml;
 
 
-function showModalWindow(employee){
-  
-  let modalHtml = "";
-      modalHtml =
-  ` <div class="modal">
-    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-    <div class="modal-info-container">
-       <img class="modal-img" src=photo alt="profile picture">
-       <h3 id="name" class="modal-name cap"></h3>
-       <p class="modal-text"></p>
-       <p class="modal-text cap"></p>
-       <hr>
-       <p class="modal-text">(555) 555-5555</p>
-       <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-       <p class="modal-text">Birthday: 10/21/2015</p>
-        </div>`;
+}
 
-    body.insertAdjacentHTML('beforeend', modalHtml);
+                 
+ 
+  gallery.addEventListener("click", (e) => {
+    const card = e.target.closest(".card");
+    const index = card.getAttribute("data-index");
+   modalIndex = index;
+   createModal(index);
+  }); 
+ 
+          
+             
       
 
-        /*const openModal = document.getElementById('open');
-        const modalContainer = document.getElementByClass('modal-info-container');
-        const closeBtn = document.getElementById('modal-close-btn')
+      
 
-        openModal.addEventListener('click', ()=>{
-          modalContainer.classList.add('show');
-        })
-
-        closeBtn.addEventListener('click', () => {
-          modalContainer.classList.remove('show');
-        })
-
-          */
-        }
+      
  
 
       
     
 
+       
 
 
 
