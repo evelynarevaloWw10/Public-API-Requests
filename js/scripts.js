678cc1afe0b62654036614ab7c438b47b33c20a8
Evelyn Arevalo <evelynarevalow@gmail.com>
2022-07-09 17:45:20 -0700

    Continue edits to function

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,30 @@
 const url = "https://randomuser.me/api/?results=12&inc=name,picture,email,location,&noinfo&nat=US"
 const searchContainer = document.querySelector(".search-container");
 const gallery = document.getElementById('gallery');   
+let body = document.querySelector('body')
 
+
+//users to display, for loop shows 12 users and allows html and JavaSript to interact w/ each other dynamically
+
+function usersToDisplay(user){
+ for(let i = 0 ; i < user.length; i++ ){
+  let html = 
+              ` <div class="card">
+                <div class="card-img-container">
+                        <img class="card-img" src=${user[i].picture.large} alt="profile picture">
+                    </div>
+                    <div class="card-info-container">
+                        <h3 id="name" class="card-name cap">${user[i].name.first}</h3>
+                        <p class="card-text">${user[i].email}</p>
+                        <p class="card-text cap">${user[i].location.city} , ${user[i].location.state}</p>
+                    </div>`
+
+                   
+
+
+ gallery.insertAdjacentHTML('beforeend', html);
+    }
+}
 
 fetch(url)
 .then(response => response.json())
@@ -9,58 +32,62 @@ fetch(url)
 .then(usersToDisplay)
 .catch((err) => console.log('SOMETHING WENT WRONG!'));
 
+// Modula 
 
 
- //gallery.innerHTML = '';
+function showModalWindow(employee){
+  
+  let modalHtml = "";
+      modalHtml =
+  ` <div class="modal">
+    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+    <div class="modal-info-container">
+       <img class="modal-img" src=photo alt="profile picture">
+       <h3 id="name" class="modal-name cap"></h3>
+       <p class="modal-text"></p>
+       <p class="modal-text cap"></p>
+       <hr>
+       <p class="modal-text">(555) 555-5555</p>
+       <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+       <p class="modal-text">Birthday: 10/21/2015</p>
+        </div>`;
 
+    body.insertAdjacentHTML('beforeend', modalHtml);
+      
 
-//users to display
-function usersToDisplay(user){
+        /*const openModal = document.getElementById('open');
+        const modalContainer = document.getElementByClass('modal-info-container');
+        const closeBtn = document.getElementById('modal-close-btn')
+
+        openModal.addEventListener('click', ()=>{
+          modalContainer.classList.add('show');
+        })
+
+        closeBtn.addEventListener('click', () => {
+          modalContainer.classList.remove('show');
+        })
+
+          */
+        }
  
-  for(let i = 0 ; i < user.length; i++ ){
-  let html = 
-    `<div class="card-img-container">
-                        <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
-                    </div>
-                    <div class="card-info-container">
-                        <h3 id="name" class="card-name cap">name</h3>
-                        <p class="card-text">email</p>
-                        <p class="card-text cap">city, state</p>
-                    </div>`
- gallery.insertAdjacentHTML('beforeend', html);
-    }
-}
-/* Modula stuff already 
 
-const employee = `<div class="modal">
-                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                    <div class="modal-info-container">
-                        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-                        <h3 id="name" class="modal-name cap">name</h3>
-                        <p class="modal-text">email</p>
-                        <p class="modal-text cap">city</p>
-                        <hr>
-                        <p class="modal-text">(555) 555-5555</p>
-                        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-                        <p class="modal-text">Birthday: 10/21/2015</p>
-                         </div>`;
- searchContainer.insertAdjacentHTML('beforeend','employee');
-*/
+      
+    
+
+
+
+
+
  
 
-/*fetch("https://randomuser.me/api/?results=12&inc=name,picture,email,location,&noinfo&nat=US")
-.then(response => response.json())
-.then(data => data.results)
-.then(usersToDisplay)
-.catch((err) => console.log('SOMETHING WENT WRONG!'));
-*/
+//event listeners 
 
-//  console.log(err);
-//});
-//console.log(response.statusText)
-//.then(response => response.json())
-//.then(data => usersToDisplay(data.results));
 
-//fetch(url)
-  //.then(response => response.json())
-  //.then(data => console.log(data));
+
+
+
+
+
+
+
+ 
